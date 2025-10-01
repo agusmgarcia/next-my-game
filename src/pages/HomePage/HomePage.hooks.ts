c53a2a06409b214f2f83ffef3fca82e8b87c2b5c
Agus Garcia <agusss.garcia19@gmail.com>
2025-10-01 10:21:37 -0300

    feat(HomePage): add main loop

--- src/pages/HomePage/HomePage.hooks.ts
+++ src/pages/HomePage/HomePage.hooks.ts
@@ -1,9 +1,46 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import { type Scene } from "#src/entities";
+import { TestScene } from "#src/scenes";
 
 import type HomePageProps from "./HomePage.types";
 
 export default function useHomePage(props: HomePageProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const sceneRef = useRef<Scene>(null);
 
-  return { ...props, canvasRef };
+  const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    sceneRef.current = new TestScene();
+  }, []);
+
+  useEffect(() => {
+    const scene = sceneRef.current;
+    if (!scene) return;
+
+    setLoading(true);
+    scene
+      .load()
+      .then(() => setLoaded(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    const scene = sceneRef.current;
+    if (!scene) return;
+
+    if (!loaded || loading) return;
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    scene.init(canvas);
+    scene.run(24);
+
+    return () => scene.dispose();
+  }, [loaded, loading]);
+
+  return { ...props, canvasRef, loaded, loading };
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,12 +2,14 @@ import useHomePage from "./HomePage.hooks";
 import type HomePageProps from "./HomePage.types";
 
 export default function HomePage(props: HomePageProps) {
-  const { canvasRef, ...rest } = useHomePage(props);
+  const { canvasRef, loaded, loading, ...rest } = useHomePage(props);
 
   return (
     <div {...rest} className="relative h-dvh w-screen overflow-hidden">
+      {loading && <p>Loading...</p>}
+
       {/* CANVAS */}
-      <canvas ref={canvasRef} className="size-full" />
+      {!loading && loaded && <canvas ref={canvasRef} className="size-full" />}
 
       {/* VERSION */}
       <p className="absolute right-2 bottom-2 bg-transparent font-sans text-base text-white">
